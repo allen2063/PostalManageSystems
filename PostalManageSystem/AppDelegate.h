f6fcef630767c5ca8da9b677ab6008366eb27e2e
allen2063 <199128927@qq.com>
2015-08-18 11:26:27 +0800

    Initial Commit

---
+++ PostalManageSystem/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PostalManageSystem
+//
+//  Created by ZengYifei on 15/8/18.
+//  Copyright (c) 2015年 IOS-developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PostalManageSystemTests/PostalManageSystemTests.m
@@ -0,0 +1,40 @@
+//
+//  PostalManageSystemTests.m
+//  PostalManageSystemTests
+//
+//  Created by ZengYifei on 15/8/18.
+//  Copyright (c) 2015年 IOS-developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface PostalManageSystemTests : XCTestCase
+
+@end
+
+@implementation PostalManageSystemTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
